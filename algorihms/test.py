c2a1847b82795590a6f1d12edfd6b1e080844778
Pedro Santos <pedroamaralsantos@ua.pt>
2018-06-12 23:29:07 +0100

    added code to extract features from audio; added git ignore

---
+++ algorihms/test.py
@@ -0,0 +1,92 @@
+import glob
+import os
+import librosa
+import numpy as np
+import matplotlib.pyplot as plt
+#import tensorflow as tf
+from matplotlib.pyplot import specgram
+
+
+def load_sound_files(file_paths):
+    raw_sounds = []
+    for fp in file_paths:
+        X,sr = librosa.load(fp)
+        raw_sounds.append(X)
+    return raw_sounds
+
+def plot_waves(sound_names,raw_sounds):
+    i = 1
+    fig = plt.figure(figsize=(25,60), dpi = 900)
+    for n,f in zip(sound_names,raw_sounds):
+        plt.subplot(10,1,i)
+        librosa.display.waveplot(np.array(f),sr=22050)
+        plt.title(n.title())
+        i += 1
+    plt.suptitle("Figure 1: Waveplot",x=0.5, y=0.915,fontsize=18)
+    plt.show()
+
+def plot_specgram(sound_names,raw_sounds):
+    i = 1
+    fig = plt.figure(figsize=(25,60), dpi = 900)
+    for n,f in zip(sound_names,raw_sounds):
+        plt.subplot(10,1,i)
+        specgram(np.array(f), Fs=22050)
+        plt.title(n.title())
+        i += 1
+    plt.suptitle("Figure 2: Spectrogram",x=0.5, y=0.915,fontsize=18)
+    plt.show()
+
+def plot_log_power_specgram(sound_names,raw_sounds):
+    i = 1
+    fig = plt.figure(figsize=(25,60), dpi = 900)
+    for n,f in zip(sound_names,raw_sounds):
+        plt.subplot(10,1,i)
+        D = librosa.logamplitude(np.abs(librosa.stft(f))**2, ref_power=np.max)
+        librosa.display.specshow(D,x_axis='time' ,y_axis='log')
+        plt.title(n.title())
+        i += 1
+    plt.suptitle("Figure 3: Log power spectrogram",x=0.5, y=0.915,fontsize=18)
+    plt.show()
+
+
+def extract_feature(file_name):
+    X, sample_rate = librosa.load(file_name)
+    stft = np.abs(librosa.stft(X))
+    mfccs = np.mean(librosa.feature.mfcc(y=X, sr=sample_rate, n_mfcc=40).T,axis=0)
+    chroma = np.mean(librosa.feature.chroma_stft(S=stft, sr=sample_rate).T,axis=0)
+    mel = np.mean(librosa.feature.melspectrogram(X, sr=sample_rate).T,axis=0)
+    contrast = np.mean(librosa.feature.spectral_contrast(S=stft, sr=sample_rate).T,axis=0)
+    tonnetz = np.mean(librosa.feature.tonnetz(y=librosa.effects.harmonic(X),
+    sr=sample_rate).T,axis=0)
+    return mfccs,chroma,mel,contrast,tonnetz
+
+def parse_audio_files(parent_dir,sub_dirs,file_ext="*.wav"):
+    features, labels = np.empty((0,193)), np.empty(0)
+    for label, sub_dir in enumerate(sub_dirs):
+        for fn in glob.glob(os.path.join(parent_dir, sub_dir, file_ext)):
+            try:
+              mfccs, chroma, mel, contrast,tonnetz = extract_feature(fn)
+            except Exception as e:
+              print "Error encountered while parsing file: ", fn
+              continue
+            ext_features = np.hstack([mfccs,chroma,mel,contrast,tonnetz])
+            features = np.vstack([features,ext_features])
+            labels = np.append(labels, fn.split('/')[2].split('-')[1])
+    return np.array(features), np.array(labels, dtype = np.int)
+
+def one_hot_encode(labels):
+    n_labels = len(labels)
+    n_unique_labels = len(np.unique(labels))
+    one_hot_encode = np.zeros((n_labels,n_unique_labels))
+    one_hot_encode[np.arange(n_labels), labels] = 1
+    return one_hot_encode
+
+
+parent_dir = '../Data/UrbanSound8K/audio/Sound-Data'
+tr_sub_dirs = ["fold1","fold2"]
+ts_sub_dirs = ["fold3"]
+tr_features, tr_labels = parse_audio_files(parent_dir,tr_sub_dirs)
+ts_features, ts_labels = parse_audio_files(parent_dir,ts_sub_dirs)
+
+tr_labels = one_hot_encode(tr_labels)
+ts_labels = one_hot_encode(ts_labels)

e41cf6d9e878677d66176221556e2fb547f2ae44
Pedro Santos <pedroamaralsantos@ua.pt>
2018-06-16 01:43:06 +0100

    create module to put commun functions of the algorithm and separate the code of the machine learning algorithm to different files

---
+++ algorihms/NeuralNetwork.py
@@ -0,0 +1,41 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score
+from sklearn import tree
+import sys
+import ControlCenter
+from sklearn.neural_network import MLPClassifier
+
+
+
+if len(sys.argv)==1:
+    features,labels = ControlCenter.getLabelsAndFeatures(1)
+else:
+    features,labels = ControlCenter.getLabelsAndFeatures(0)
+
+
+
+
+sizeOfTrainSet= int(len(features)*0.6)
+sizeOfCrossValidationSet = int((len(features)-sizeOfTrainSet)/2)
+sizeOfTestSet = len(features)- sizeOfCrossValidationSet - sizeOfTrainSet
+
+
+train_set_features = features[0:sizeOfTrainSet]
+train_set_labels = labels[0:sizeOfTrainSet]
+
+CrossValidation_set_features = features[sizeOfTrainSet:sizeOfTrainSet+sizeOfCrossValidationSet]
+CrossValidation_set_labels = labels[sizeOfTrainSet:sizeOfTrainSet+sizeOfCrossValidationSet]
+
+test_set_features = features[sizeOfTrainSet+sizeOfCrossValidationSet:sizeOfTrainSet+sizeOfCrossValidationSet+sizeOfTestSet]
+test_set_labels = labels[sizeOfTrainSet:sizeOfTrainSet+sizeOfCrossValidationSet]
+
+
+
+#print(tr_features.shape[1])
+clf = MLPClassifier(solver='lbfgs', alpha=1e-5,hidden_layer_sizes=(280, 300), random_state=1,learning_rate_init=0.01)
+clf.fit(train_set_features, train_set_labels)
+y_pred=clf.predict(CrossValidation_set_features)
+print(y_pred)
+accuracy = accuracy_score(y_pred, CrossValidation_set_labels)
+print("Accuracy MLPClassifier: %f" % (accuracy*100.0))
---
+++ algorihms/decisionTree.py
@@ -0,0 +1,42 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score
+from sklearn import tree
+import sys
+import ControlCenter
+from sklearn.neural_network import MLPClassifier
+
+
+
+if len(sys.argv)==1:
+    features,labels = ControlCenter.getLabelsAndFeatures(1)
+else:
+    features,labels = ControlCenter.getLabelsAndFeatures(0)
+
+
+
+
+sizeOfTrainSet= int(len(features)*0.6)
+sizeOfCrossValidationSet = int((len(features)-sizeOfTrainSet)/2)
+sizeOfTestSet = len(features)- sizeOfCrossValidationSet - sizeOfTrainSet
+
+
+train_set_features = features[0:sizeOfTrainSet]
+train_set_labels = labels[0:sizeOfTrainSet]
+
+CrossValidation_set_features = features[sizeOfTrainSet:sizeOfTrainSet+sizeOfCrossValidationSet]
+CrossValidation_set_labels = labels[sizeOfTrainSet:sizeOfTrainSet+sizeOfCrossValidationSet]
+
+test_set_features = features[sizeOfTrainSet+sizeOfCrossValidationSet:sizeOfTrainSet+sizeOfCrossValidationSet+sizeOfTestSet]
+test_set_labels = labels[sizeOfTrainSet:sizeOfTrainSet+sizeOfCrossValidationSet]
+
+
+print(len(train_set_features.tolist()))
+print(train_set_labels.shape[0])
+clf = tree.DecisionTreeClassifier()
+clf.fit(train_set_features.tolist(), train_set_labels.tolist())
+
+y_pred=clf.predict(CrossValidation_set_features.tolist())
+print(y_pred)
+accuracy = accuracy_score(y_pred, CrossValidation_set_labels.tolist())
+print("Accuracy DecisionTreeClassifier: %f" % (accuracy*100.0))

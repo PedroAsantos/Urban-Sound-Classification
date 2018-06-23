f7cb98e2ce7de7e1096886a29b88afc002e4c092
jpc <jp.castanheira@ua.pt>
2018-06-23 23:36:20 +0100

    Added more developed code for ANN and a graph

--- algorihms/NeuralNetwork.py
+++ algorihms/NeuralNetwork.py
@@ -7,7 +7,9 @@ import ControlCenter
 from sklearn.neural_network import MLPClassifier
 from sklearn.utils import shuffle
 from mpl_toolkits.mplot3d import Axes3D
-
+from sklearn.metrics import confusion_matrix
+from matplotlib import cm
+from mpl_toolkits.mplot3d import axes3d
 
 if len(sys.argv)==1:
     features,labels = ControlCenter.getLabelsAndFeatures(1)
@@ -31,7 +33,6 @@ CrossValidation_set_labels = labels[sizeOfTrainSet:sizeOfTrainSet+sizeOfCrossVal
 
 test_set_features = features[sizeOfTrainSet+sizeOfCrossValidationSet:sizeOfTrainSet+sizeOfCrossValidationSet+sizeOfTestSet]
 test_set_labels = labels[sizeOfTrainSet:sizeOfTrainSet+sizeOfCrossValidationSet]
-
 
 
 #
@@ -62,22 +63,36 @@ def MPL_Classifier_2layer(neurons1, neurons2):
 # This is the "main" part of the code
 #
 
-#print(tr_features.shape[1])
 neurons = [25, 50, 100, 150, 200, 500]
 accuracy_list_1layer = []
 accuracy_list_2layer = []
 
 ### Testing one hidden layer ###
-### Uncomment this block to test
-#i = 0
-#while i < len(neurons):
-#	print(neurons[i])
-#	accuracy_list_1layer.append(MPL_Classifier_1layer(neurons[i]))
-#	i+=1
-#i = 0
-#while i < len(accuracy_list_1layer):
-#	print(accuracy_list_1layer[i])
-#	i+=1
+i = 0
+while i < len(neurons):
+	print(neurons[i])
+	accuracy_list_1layer.append(MPL_Classifier_1layer(neurons[i]))
+	i+=1
+i = 0
+while i < len(accuracy_list_1layer):
+	print(accuracy_list_1layer[i])
+	i+=1
+
+### Confusion Matrix for 1000 neurons
+print("confusion_matrix:")
+clf = MLPClassifier(solver='lbfgs', alpha=1e-5,hidden_layer_sizes=(1000), random_state=1,learning_rate_init=0.01)
+clf.fit(train_set_features, train_set_labels)
+y_pred=clf.predict(CrossValidation_set_features)
+print(confusion_matrix(CrossValidation_set_labels, y_pred))
+
+### Ploting results (2D) ###
+plt.plot(neurons, accuracy_list_1layer)
+plt.ylim(0,100)
+plt.scatter(neurons, accuracy_list_1layer)
+plt.grid()
+plt.xlabel('Number of neurons')
+plt.ylabel('Accuracy')
+plt.show()
 
 
 ### Testing with two hidden layers ###
@@ -92,29 +107,22 @@ while i < len(neurons):
 	i+=1
 
 i = 0
-while i < len(accuracy_list_1layer):
-	print(accuracy_list_1layer[i])
+while i < len(accuracy_list_2layer):
+	print(accuracy_list_2layer[i])
 	i+=1
-
-
-### Ploting results (2D) ###
-#plt.plot(neurons, accuracy_list_1layer)
-#plt.ylim(0,100)
-#plt.scatter(neurons, accuracy_list_1layer)
-#plt.grid()
-#plt.xlabel('Number of neurons')
-#plt.ylabel('Accuracy')
-#plt.show()
 
 ### Ploting results (3D) ###
 
-soa = np.array([neurons, neurons, accuracy_list_2layer])
-
-X, Y, Z, U, V, W = zip(*soa)
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-ax.quiver(X, Y, Z, U, V, W)
+
+X = [25, 25, 25, 25, 25, 25, 50, 50, 50, 50, 50, 50, 100, 100, 100, 100, 100, 100, 150, 150, 150, 150, 150, 150, 200, 200, 200, 200, 200, 200, 500, 500, 500, 500, 500, 500]
+Y = [25, 50, 100, 150, 200, 500, 25, 50, 100, 150, 200, 500, 25, 50, 100, 150, 200, 500, 25, 50, 100, 150, 200, 500, 25, 50, 100, 150, 200, 500, 25, 50, 100, 150, 200, 500]
+
+ax.scatter(X, Y, accuracy_list_2layer)
+
 ax.set_xlabel('Neurons in first layer')
 ax.set_ylabel('Neurons in second layer')
 ax.set_zlabel('Accuracy')
+
 plt.show()
